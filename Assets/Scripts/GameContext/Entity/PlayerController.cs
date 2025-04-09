83b6b6a0ede203a11a3dfb1869760a79d4013e97
Vadim Yevtushenko <vadim.y@postphysical.io>
2025-04-09 21:15:49 +0300

    Added: Player - Player movement

--- Assets/Scripts/GameContext/Entity/PlayerController.cs
+++ Assets/Scripts/GameContext/Entity/PlayerController.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Player _playerPrefab;
+    [SerializeField] private InputActionAsset Input;
     
     public Player Player { get; private set; }
     
@@ -11,6 +13,39 @@ public class PlayerController : MonoBehaviour
         if (Player == null)
         {
             Player = Instantiate(_playerPrefab, transform);
+        }
+
+        var map = Input.FindActionMap("GameplayMap");
+        map.Enable();
+    }
+
+    public void Update()
+    {
+        var moveUp = Input.FindAction("MoveUp");
+        var moveDown = Input.FindAction("MoveDown");
+        var moveLeft = Input.FindAction("MoveLeft");
+        var moveRight = Input.FindAction("MoveRight");
+        
+        var isMoveUp = moveUp.IsPressed();
+        var isMoveDown = moveDown.IsPressed();
+        var isMoveLeft = moveLeft.IsPressed();
+        var isMoveRight = moveRight.IsPressed();
+
+        if (isMoveUp)
+        {
+            Player.transform.position += new Vector3(0, 0.01f);
+        }
+        if (isMoveDown)
+        {
+            Player.transform.position += new Vector3(0, -0.01f);
+        }
+        if (isMoveLeft)
+        {
+            Player.transform.position += new Vector3(-0.01f, 0);
+        }
+        if (isMoveRight)
+        {
+            Player.transform.position += new Vector3(0.01f, 0);
         }
     }
 }
