df3c5ac63c8116121bf3450b31f10791afd9d12b
Vadim Yevtushenko <vadim.y@postphysical.io>
2025-04-11 03:37:07 +0300

    Added: Gameplay - Board

--- Assets/Scripts/GameContext/Entity/Entity.cs
+++ Assets/Scripts/GameContext/Entity/Entity.cs
@@ -4,7 +4,11 @@ public class Entity : MonoBehaviour
 {
     private const float PlayerSpeed = 5f;
 
+    private static Vector2 DefaultColliderSize = new Vector2(0.28f, 0.28f);
+    private static Vector2 SideColliderSize = new Vector2(0.2f, 0.28f);
+
     [SerializeField] private SpriteRenderer _spriteRenderer;
+    [SerializeField] private BoxCollider2D _boxCollider2D;
     [SerializeField] private Rigidbody2D _rigidbody;
     [SerializeField] private Sprite _defaultSprite;
     [SerializeField] private Sprite _leftSprite;
@@ -17,14 +21,17 @@ public class Entity : MonoBehaviour
         if (Direction.x < 0)
         {
             _spriteRenderer.sprite = _leftSprite;
+            _boxCollider2D.size = SideColliderSize;
         }
         else if (Direction.x > 0)
         {
             _spriteRenderer.sprite = _rightSprite;
+            _boxCollider2D.size = SideColliderSize;
         }
         else
         {
             _spriteRenderer.sprite = _defaultSprite;
+            _boxCollider2D.size = DefaultColliderSize;
         }
         
         _rigidbody.MovePosition(_rigidbody.position + (Vector2)Direction.normalized * PlayerSpeed * Time.fixedDeltaTime);
